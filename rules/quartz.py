121780097aefbc2184e61c6d6d46cfdf9d1319d0
Yutao Yuan <yyt16384@gmail.com>
2016-05-26 19:30:44 +0800

    Add quartz

---
+++ rules/quartz.py
@@ -0,0 +1,9 @@
+textures = {
+    'quartz_block': ('quartz', 'ore_block'),
+    'quartz_chiseled': ('quartz', 'quartz_chiseled'),
+    'quartz_crystal_full': ('quartz', 'ore_crystal', 'ore_crystal'),
+    'quartz_crystal_piece': ('quartz', 'fragment', 'fragment'),
+    'quartz_ore': ('quartz', 'simple_block', 'mineral'),
+    'quartz_pillar_side': ('quartz', 'quartz_pillar_side'),
+    'quartz_pillar_top': ('quartz', 'quartz_pillar_top'),
+}
