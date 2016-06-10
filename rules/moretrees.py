cd47d37469f70ad0b33a59121d72b162244ec998
Yutao Yuan <yyt16384@gmail.com>
2016-06-10 18:30:17 +0800

    Add beech

--- rules/moretrees.py
+++ rules/moretrees.py
@@ -4,4 +4,9 @@ textures = {
     'moretrees_apple_tree_trunk': ('apple_tree_tree', 'tree'),
     'moretrees_apple_tree_trunk_top': (('apple_tree_tree', 'apple_tree_wood'), 'tree_top'),
     'moretrees_apple_tree_wood': ('apple_tree_wood', 'wood'),
+    'moretrees_beech_leaves': ('beech_leaves', 'simple_block', 'leaves'),
+    'moretrees_beech_sapling': (('beech_tree', 'beech_leaves'), 'sapling', 'sapling'),
+    'moretrees_beech_trunk': ('beech_tree', 'tree'),
+    'moretrees_beech_trunk_top': (('beech_tree', 'beech_wood'), 'tree_top'),
+    'moretrees_beech_wood': ('beech_wood', 'wood'),
 }
