d277c2020e4e9623dbe94d7286d0a259f0bad1eb
Yutao Yuan <yyt16384@gmail.com>
2016-06-10 18:11:38 +0800

    Add apple tree

---
+++ rules/moretrees.py
@@ -0,0 +1,7 @@
+textures = {
+    'moretrees_apple_tree_leaves': ('apple_tree_leaves', 'simple_block', 'leaves'),
+    'moretrees_apple_tree_sapling': (('apple_tree_tree', 'apple_tree_leaves'), 'sapling', 'sapling'),
+    'moretrees_apple_tree_trunk': ('apple_tree_tree', 'tree'),
+    'moretrees_apple_tree_trunk_top': (('apple_tree_tree', 'apple_tree_wood'), 'tree_top'),
+    'moretrees_apple_tree_wood': ('apple_tree_wood', 'wood'),
+}
