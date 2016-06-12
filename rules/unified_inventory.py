a66a5e07bba7983f234dc29b4c06435fdfa98b91
Yutao Yuan <yyt16384@gmail.com>
2016-06-12 18:17:59 +0800

    Fix unified inventory textures

--- rules/unified_inventory.py
+++ rules/unified_inventory.py
@@ -1,13 +1,6 @@
-_none_texture = ('color_black', 'simple_block', 'transparent')
-
-
 textures = {
-    'ui_bags_lg_form': _none_texture,
-    'ui_bags_main_form': _none_texture,
-    'ui_bags_med_form': _none_texture,
-    'ui_bags_sm_form': _none_texture,
-    'ui_crafting_form': _none_texture,
+    'ui_crafting_form': (('listcolors_slot_bg_normal', 'color_gray'), 'ui_crafting_form', 'ui_crafting_form'),
     'ui_form_bg': 'gui_formbg',
-    'ui_main_inventory': _none_texture,
-    'ui_single_slot': _none_texture,
+    'ui_main_inventory': ('listcolors_slot_bg_normal', 'ui_main_inventory', 'ui_main_inventory'),
+    'ui_single_slot': ('listcolors_slot_bg_normal', 'ui_single_slot', 'ui_single_slot'),
 }
