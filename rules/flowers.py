c0ef4b628cd87e854c47825c30d9711465f44b72
Yutao Yuan <yyt16384@gmail.com>
2016-06-14 22:02:32 +0800

    Add some flowers

--- rules/flowers.py
+++ rules/flowers.py
@@ -1,3 +1,9 @@
 textures = {
+    'flowers_dandelion_white': (('dandelion_white', 'grass'), 'flower', 'flower'),
+    'flowers_dandelion_yellow': (('dandelion_yellow', 'grass'), 'flower', 'flower'),
+    'flowers_geranium': (('geranium', 'grass'), 'flower', 'flower'),
+    'flowers_rose': (('rose', 'grass'), 'flower', 'flower'),
+    'flowers_tulip': (('tulip', 'grass'), 'flower', 'flower'),
+    'flowers_viola': (('viola', 'grass'), 'flower', 'flower'),
     'flowers_waterlily': (('waterlily_1', 'waterlily_2'), 'waterlily', 'waterlily'),
 }
