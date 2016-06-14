c4974e882b07d4ef05389655df192513fa45c94b
Yutao Yuan <yyt16384@gmail.com>
2016-06-14 22:19:23 +0800

    Add mushrooms

--- rules/flowers.py
+++ rules/flowers.py
@@ -2,6 +2,8 @@ textures = {
     'flowers_dandelion_white': (('dandelion_white', 'grass'), 'flower', 'flower'),
     'flowers_dandelion_yellow': (('dandelion_yellow', 'grass'), 'flower', 'flower'),
     'flowers_geranium': (('geranium', 'grass'), 'flower', 'flower'),
+    'flowers_mushroom_brown': (('mushroom_brown', 'mushroom_stem'), 'mushroom', 'mushroom'),
+    'flowers_mushroom_red': (('mushroom_red', 'mushroom_stem'), 'mushroom', 'mushroom'),
     'flowers_rose': (('rose', 'grass'), 'flower', 'flower'),
     'flowers_tulip': (('tulip', 'grass'), 'flower', 'flower'),
     'flowers_viola': (('viola', 'grass'), 'flower', 'flower'),
