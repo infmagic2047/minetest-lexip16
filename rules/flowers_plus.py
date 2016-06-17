d451d312193ac233433068d4eff0bbd4c555195b
Yutao Yuan <yyt16384@gmail.com>
2016-06-17 23:48:51 +0800

    Add flowers_plus waterlily textures

---
+++ rules/flowers_plus.py
@@ -0,0 +1,9 @@
+textures = {
+    'flowers_waterlily_22.5': 'flowers_waterlily',
+    'flowers_waterlily_45': 'flowers_waterlily',
+    'flowers_waterlily_67.5': 'flowers_waterlily',
+    'flowers_waterlily_small_1': 'flowers_waterlily',
+    'flowers_waterlily_small_2': 'flowers_waterlily',
+    'flowers_waterlily_small_3': 'flowers_waterlily',
+    'flowers_waterlily_small_4': 'flowers_waterlily',
+}
