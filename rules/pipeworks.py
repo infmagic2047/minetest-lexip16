7d06dc61c17ed2992b343b1979113f77d0cbf650
Yutao Yuan <yyt16384@gmail.com>
2016-06-18 00:07:45 +0800

    Add pipeworks tube connection

---
+++ rules/pipeworks.py
@@ -0,0 +1,5 @@
+textures = {
+    'pipeworks_tube_connection_metallic': (('steel', 'color_black'), 'tube_connection', 'tube_connection'),
+    'pipeworks_tube_connection_stony': (('stone', 'color_black'), 'tube_connection', 'tube_connection'),
+    'pipeworks_tube_connection_wooden': (('wood', 'color_black'), 'tube_connection', 'tube_connection'),
+}
