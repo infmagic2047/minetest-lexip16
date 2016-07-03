693353b8f1db88afa7877289f9e9b116fa44f9df
Yutao Yuan <yyt16384@gmail.com>
2016-07-03 09:33:28 +0800

    Add mesecons wire

---
+++ rules/mesecons.py
@@ -0,0 +1,5 @@
+textures = {
+    'mesecons_wire_inv': ('mesecons', 'simple_block', 'wire_inv'),
+    'mesecons_wire_off': ('mesecons', 'simple_block'),
+    'mesecons_wire_on': ('mesecons_on', 'simple_block'),
+}
