a654ec6f5917675459d13ac038fe53c14750b32a
Yutao Yuan <yyt16384@gmail.com>
2016-12-03 22:34:31 +0800

    Rename rails

---
+++ rules/carts.py
@@ -0,0 +1,6 @@
+textures = {
+    'carts_rail_crossing': (('wood', 'steel'), 'rail_crossing', 'rail_crossing'),
+    'carts_rail_curved': (('wood', 'steel'), 'rail_curved', 'rail_curved'),
+    'carts_rail_straight': (('wood', 'steel'), 'rail', 'rail'),
+    'carts_rail_t_junction': (('wood', 'steel'), 'rail_t_junction', 'rail_t_junction'),
+}
