d6cab95943a40f9841b04e26e2d23809dc3fe699
Yutao Yuan <infmagic2047reg@outlook.com>
2020-06-12 20:20:54 +0800

    Add some flowers and flowers_plus textures

--- rules/flowers.py
+++ rules/flowers.py
@@ -1,4 +1,5 @@
 textures = {
+    "flowers_chrysanthemum_green": (("chrysanthemum_green", "grass"), "flower", "flower"),
     "flowers_dandelion_white": (("dandelion_white", "grass"), "flower", "flower"),
     "flowers_dandelion_yellow": (("dandelion_yellow", "grass"), "flower", "flower"),
     "flowers_geranium": (("geranium", "grass"), "flower", "flower"),
@@ -6,6 +7,7 @@ textures = {
     "flowers_mushroom_red": (("mushroom_red", "mushroom_stem"), "mushroom", "mushroom"),
     "flowers_rose": (("rose", "grass"), "flower", "flower"),
     "flowers_tulip": (("tulip", "grass"), "flower", "flower"),
+    "flowers_tulip_black": (("tulip_black", "grass"), "flower", "flower"),
     "flowers_viola": (("viola", "grass"), "flower", "flower"),
     "flowers_waterlily": (("waterlily_1", "waterlily_2"), "waterlily", "waterlily"),
     "flowers_waterlily_bottom": ("waterlily_1", "simple_block", "waterlily_bottom"),
--- rules/flowers_plus.py
+++ rules/flowers_plus.py
@@ -1,4 +1,8 @@
 textures = {
+    "flowers_seaweed": ("seaweed", "simple_block", "seaweed"),
+    "flowers_seaweed_2": "flowers_seaweed",
+    "flowers_seaweed_3": "flowers_seaweed",
+    "flowers_seaweed_4": "flowers_seaweed",
     "flowers_waterlily_22.5": "flowers_waterlily",
     "flowers_waterlily_45": "flowers_waterlily",
     "flowers_waterlily_67.5": "flowers_waterlily",
